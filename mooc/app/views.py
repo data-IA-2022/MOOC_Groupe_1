d1c460a78a7697130858af670ecc712ab68b8c74
Matthieu Ernoux <matt.er41@hotmail.fr>
2023-03-09 11:09:41 +0100

    ajout des différentes pages plus création des test

--- mooc/app/views.py
+++ mooc/app/views.py
@@ -4,6 +4,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
+from utils import connect_ssh_tunnel, connect_to_db, relative_path
+import pandas as pd
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -19,4 +23,28 @@ def sentiments(request):
     return render(request, 'sentiments.html')
 
 def score(request):
-    return render(request, 'score.html')
+    path_config = relative_path("config.yaml")
+
+    ssh = connect_ssh_tunnel(path_config, 'ssh_mysql')
+
+    mysqlEngine = connect_to_db(path_config, 'database_mysql')
+    mysqlConn = mysqlEngine.connect()
+    mysqlConn.begin()
+
+    df = pd.read_sql('Users', mysqlConn)
+    df_city = df['city'].unique().tolist()
+    df_country = df["country"]
+    df_level_of_education = df["level_of_education"]
+    
+    from django import forms
+
+    class CityForm(forms.Form):
+        city_choices = df_city
+        city = forms.ChoiceField(choices=city_choices, required=True)
+
+    city_form = CityForm()
+
+    print(df)
+
+    context = {'city_form': city_form}
+    return render(request, 'score.html', context)
