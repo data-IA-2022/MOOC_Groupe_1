1decb701a02632853cea372cebd06847a3747b36
Matthieu Ernoux <matt.er41@hotmail.fr>
2023-03-10 13:25:25 +0100

    commit final

--- mooc/app/views.py
+++ mooc/app/views.py
@@ -1,11 +1,11 @@
 
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+import pickle
 
-
-from utils import connect_ssh_tunnel, connect_to_db, relative_path
 import pandas as pd
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from utils import connect_ssh_tunnel, connect_to_db, relative_path
 
 
 def home(request):
@@ -32,14 +32,62 @@ def score(request):
     mysqlConn.begin()
 
     df = pd.read_sql('Users', mysqlConn)
+    df_cours = pd.read_sql('Course',mysqlConn)
     ssh.close()
     df_city = df['city'].unique().tolist()
 
     df_country = df["country"].unique().tolist()
     df_level_of_education = df["level_of_education"].unique().tolist()
-   
+    df_cours = df_cours['id'].unique().tolist()
 
     context = {'city': df_city,
                "country":df_country,
-               "level_of_education":df_level_of_education}
+               "level_of_education":df_level_of_education,
+               "course_id":df_cours,}
+    
+    
+    if request.method == 'POST':
+
+        body = request.POST.get('body','')
+        gender = request.POST.get('gender')
+        year_of_birth = request.POST.get('year_of_birth')
+        city = request.POST.get('city','')
+        country = request.POST.get('country','')
+        level_of_education = request.POST.get('level_of_education')
+        nb_messages = request.POST.get('nb_messages')
+        course_id = request.POST.get('course_id')
+    
+        from textblob import Blobber
+        from textblob_fr import PatternAnalyzer, PatternTagger
+
+        tb = Blobber(pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
+
+        print(body)
+
+        blob = tb(body)
+        polarity, subjectivity = blob.sentiment
+        
+        # do something with the input data
+        df = pd.DataFrame({
+                            'gender' : gender ,
+                            'year_of_birth' : year_of_birth,
+                            'city' : city,
+                            'country' : country,
+                            'level_of_education' : level_of_education,
+                            "polarity" : polarity,
+                            "subjectivity" : subjectivity,
+                            "nb_messages" : nb_messages,
+                            "course_id" : course_id,
+                            
+                            }, index=[0])
+
+        print(df)
+
+        model = pickle.load(open(relative_path('GIGAMODEL.model'), 'rb'))
+
+        result = round(model.predict(df)[0], 2)
+
+        context['result'] = result
+        print(context['result'])
+
     return render(request, 'score.html', context)
